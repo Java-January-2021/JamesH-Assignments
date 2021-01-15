2aa3918ce6b09659f68ba0fa28b7192041e7efcc
James Haltiwanger <jhalt@bellsouth.net>
2021-01-14 19:30:58 -0500

    first commit

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is James Haltiwanger");
+        System.out.println("I am 31 years old");
+        System.out.println("My home town is Jacksonville, Fl");
+    }
+}
