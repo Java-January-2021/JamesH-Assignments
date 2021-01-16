248fdfe15240e6ab475e9e6a280e399d9f00d199
James Haltiwanger <jhalt@bellsouth.net>
2021-01-15 22:00:33 -0500

    first commit

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,6 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzzIt = new FizzBuzz();
+        System.out.println(fizzBuzzIt.fizzBuzz(16));
+    }
+}
