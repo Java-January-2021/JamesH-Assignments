6cc6695d93e67acf4b43f6a09cfdd63183f73a17
James Haltiwanger <jhalt@bellsouth.net>
2021-01-15 20:26:34 -0500

    first commit

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean findSideC = new Pythagorean();
+        System.out.println(findSideC.calculateHypotenuse(3, 4));
+    }
+}
