e73a290bcc71b660757f8da335921c7646a1c879
James Haltiwanger <jhalt@bellsouth.net>
2021-01-17 22:22:13 -0500

    initial exceptions

---
+++ 01-java-fundamentals/08-lists-of-exceptions/ListOfExceptions.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class ListOfExceptions {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        System.out.println(myList);
+            for(int i = 0; i < myList.size(); i++) {
+            try{     
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(castedValue);
+            }
+            catch(Exception error) {
+                System.out.println("Exception error at: " + i);
+            }
+        }
+        System.out.println("End of program...");
+    }
+}
