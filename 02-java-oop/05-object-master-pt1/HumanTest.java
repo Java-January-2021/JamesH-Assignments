256c6386a4a9fe6d4f7b2cce39de270c2e53e13f
James Haltiwanger <jhalt@bellsouth.net>
2021-01-21 18:26:08 -0500

    initial object master part 1

---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,7 @@
+public class HumanTest {
+    public static void main(String[] args) {
+        Human wizard = new Human();
+        Human ninja = new Human();
+        wizard.attack(ninja);
+    }
+}
