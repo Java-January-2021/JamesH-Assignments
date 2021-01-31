75da8ea3b6957d8746c6d1b7038a69252a77ab09
James Haltiwanger <jhalt@bellsouth.net>
2021-01-31 17:27:25 -0500

    initial DojoSurvey

---
+++ 04-java-spring/01-spring-fundamentals/07-dojo-survey/DojoSurvey/src/test/java/com/jameshaltiwanger/Survey/DojoSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameshaltiwanger.Survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
