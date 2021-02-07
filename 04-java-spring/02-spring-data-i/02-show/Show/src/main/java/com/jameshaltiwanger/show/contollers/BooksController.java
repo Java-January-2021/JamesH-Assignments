f4e084ef853b32b74b15fed74f923f80f037b100
James Haltiwanger <jhalt@bellsouth.net>
2021-02-06 20:44:15 -0500

    initial Show Assignment

---
+++ 04-java-spring/02-spring-data-i/02-show/Show/src/main/java/com/jameshaltiwanger/show/contollers/BooksController.java
@@ -0,0 +1,53 @@
+package com.jameshaltiwanger.show.contollers;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jameshaltiwanger.show.models.Book;
+import com.jameshaltiwanger.show.services.BookService;
+
+@Controller
+public class BooksController {
+	
+    private final BookService bookService;
+    
+    public BooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    
+    @RequestMapping("/books")
+    public String index(Model model) {
+        List<Book> books = bookService.allBooks();
+        model.addAttribute("books", books);
+        return "index.jsp";
+    }
+    
+    @RequestMapping("/books/{index}")
+    public String findBookByIndex(Model model, @PathVariable("index") Long index) {
+        Book book = bookService.findBook(index);
+        model.addAttribute("book", book);
+        return "showBook.jsp";
+    }
+    
+    @RequestMapping("/books/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "new.jsp";
+    }
+    @RequestMapping(value="/books", method=RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "new.jsp";
+        } else {
+            bookService.createBook(book);
+            return "redirect:/books";
+        }
+    }
+}
---
+++ 04-java-spring/02-spring-data-i/02-show/Show/src/main/java/com/jameshaltiwanger/show/services/BookService.java
@@ -0,0 +1,39 @@
+package com.jameshaltiwanger.show.services;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.jameshaltiwanger.show.models.Book;
+import com.jameshaltiwanger.show.repositories.BookRepository;
+
+@Service
+public class BookService {
+    
+	// adding the book repository as a dependency
+	   private final BookRepository bookRepository;
+	    
+	    public BookService(BookRepository bookRepository) {
+	        this.bookRepository = bookRepository;
+	    }
+	    // returns all the books
+	    public List<Book> allBooks() {
+	        return bookRepository.findAll();
+	    }
+	    // creates a book
+	    public Book createBook(Book b) {
+	        return bookRepository.save(b);
+	    }
+	    // retrieves a book
+	    public Book findBook(Long id) {
+	        Optional<Book> optionalBook = bookRepository.findById(id);
+	        if(optionalBook.isPresent()) {
+	            return optionalBook.get();
+	        } else {
+	            return null;
+	        }
+	    }
+	   
+}
